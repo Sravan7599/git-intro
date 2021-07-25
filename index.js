bc36d9528c98b3634f8b2bdd679db17d4dad4026
Sravan7599 <kcmreddykummitha@gmail.com>
2021-07-25 11:01:16 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,81 @@
+// Javascript is dynamically typed
+
+// Datatypes:
+// 1.primitive datatype 
+// 2.referrential datatype
+
+
+// var a=5;
+// var b="sravan";
+// var c=true;
+// var d=null;
+// var e;
+// var symbol;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+
+
+
+// console.log(typeof a) 
+// console.log(typeof b) 
+// console.log(typeof c) 
+// console.log(typeof d)   
+// console.log(typeof e) 
+
+// // hosting
+// console.log(e)
+// var e="sravan";
+
+
+
+// Functions
+// function functionName(){
+// }
+
+
+
+// function add(a,b,c){
+//     console.log(a+b+c);
+// }
+// add(7,5,4);
+
+
+
+// fucntion
+function add(){
+    console.log("hello world!");
+}
+add();
+
+
+fucntion print(a,b){
+    var x=a+b;
+    return x;   
+}
+var c=print(a,b);
+console.log(rem);
+
+
+
+
+
+
+// // hoisting
+add();
+function add(){
+    console.log("hello world!");
+}
+
+// fuction expression
+var rem=add(2,3);
+ console.log(rem);
+     var add=function(a,b){
+         var add=a+b
+         return add;
+     }
+ var rem=add(2,3);
+ console.log(rem);
